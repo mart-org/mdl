54d1c4585064e1c0f766ef64f0d52dc930ce88c6
Jefferson Dev <jefferson.dev.js@gmail.com>
2023-11-03 08:42:27 -0500

    add configuration of sass and build the forlder app for the client

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,30 @@
 const path = require("path");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+const mainConfig = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/",
-    filename: "bundle.js",
+    filename: "library.js",
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          miniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
@@ -20,8 +33,46 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader", // Usamos el loader de Babel
+        use: "babel-loader",
       },
     ],
   },
 };
+
+const clientConfig = {
+  entry: {
+    client: "./app/main.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "client/"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          miniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
+
+// Exportamos ambas configuraciones
+module.exports = [mainConfig, clientConfig];
