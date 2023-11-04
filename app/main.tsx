d96ccf1774e20213caced47850b9204466516a8e
Jefferson Dev <jefferson.dev.js@gmail.com>
2023-11-03 19:33:30 -0500

    refactoring of code for render app

--- app/main.tsx
+++ app/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as lib from "react-dev-lib";
+// import * as lib from "react-dev-lib";
 import { Button } from "../src/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <>
-    <lib.Button text="In npm" />
+    {/* <lib.Button text="In npm" /> */}
     <Button text="Local" />
   </>
 );
