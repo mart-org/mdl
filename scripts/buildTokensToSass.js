1076363a3fd858541e2bebd151d87e11562c953e
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-13 08:18:41 -0500

    add script for converted desing tokens to sass

--- scripts/buildTokensToSass.js
+++ scripts/buildTokensToSass.js
@@ -1,5 +1,10 @@
-import { writeSassFiles } from "mart-obj-to-sass";
+import { genFilesWithStruct } from "mart-obj-to-sass";
 import { choices } from "mart-desing-token";
 
 const folder = "./packages/core/sass/setting";
-writeSassFiles(choices, folder, true);
+
+async function genFiles() {
+  await genFilesWithStruct(choices, folder, true);
+}
+
+genFiles();
